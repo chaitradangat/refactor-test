1ecf5012e4854669c363f3cb5cbc046cb5c96392
Chaitra Dangat <Chaitra.Dangat@LongandFoster.com>
2021-07-08 04:35:57 +0530

    replaced the switch case logic with dictionary rules

--- csharp/Tennis/TennisGame1.cs
+++ csharp/Tennis/TennisGame1.cs
@@ -46,22 +46,7 @@ namespace Tennis
             var tempScore = 0;
             if (m_score1 == m_score2)
             {
-                switch (m_score1)
-                {
-                    case 0:
-                        score = "Love-All";
-                        break;
-                    case 1:
-                        score = "Fifteen-All";
-                        break;
-                    case 2:
-                        score = "Thirty-All";
-                        break;
-                    default:
-                        score = "Deuce";
-                        break;
-
-                }
+                score = tie.ContainsKey(m_score1) ? tie[m_score1] : "Deuce";
             }
             else if (m_score1 >= 4 || m_score2 >= 4)
             {
@@ -73,26 +58,7 @@ namespace Tennis
             }
             else
             {
-                for (var i = 1; i < 3; i++)
-                {
-                    if (i == 1) tempScore = m_score1;
-                    else { score += "-"; tempScore = m_score2; }
-                    switch (tempScore)
-                    {
-                        case 0:
-                            score += "Love";
-                            break;
-                        case 1:
-                            score += "Fifteen";
-                            break;
-                        case 2:
-                            score += "Thirty";
-                            break;
-                        case 3:
-                            score += "Forty";
-                            break;
-                    }
-                }
+                score = notie[m_score1] + "-" + notie[m_score2];
             }
             return score;
         }
